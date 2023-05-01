c8a733dad717f1496fe480d4765414b58229b826
Vishnu <96681356+VishnuSai2000@users.noreply.github.com>
2023-05-01 21:06:10 +0530

    Delete employee directory

--- employee/src/main/java/employee/cox/employee/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package employee.cox.employee.repository;
-
-import employee.cox.employee.model.Employee;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface EmployeeRepository extends MongoRepository<Employee, Integer> {
-}
